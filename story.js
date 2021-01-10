63d05d650ebc5189b2b053cae3bcc8074f2d854f
A-Abdeen <ahmed.yahya18@gmail.com>
2021-01-10 20:20:10 +0300

    node is finally working

---
+++ story.js
@@ -0,0 +1,6 @@
+
+const username = "Mora"
+const age = 25
+const hobby = "Dancing"
+
+console.log(`Mr. ${username}, in 33 years, you will be ${age + 33} years old. By then you'll have decades of ${hobby} practice!`)
